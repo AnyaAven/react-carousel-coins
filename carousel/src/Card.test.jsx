d5b22956ac7bd0f8fc38f169673dc94c2d19d4d9
Anya Aven <leafeon96@gmail.com>
2024-05-28 19:44:43 -0400

    Removed comment

--- carousel/src/Card.test.jsx
+++ carousel/src/Card.test.jsx
@@ -1,7 +1,7 @@
 import { describe, it, test, expect } from "vitest";
 import React from "react";
 import { render } from "@testing-library/react";
-import Card from "./Card.jsx"; //TODO: do you need to add .jsx?
+import Card from "./Card.jsx";
 import image1 from "./image1.jpg";
 
 test("render without crashing", function () {
