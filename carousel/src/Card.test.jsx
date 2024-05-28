3c1807b8596d0db537a7b71d2ab8eb34898a9dfa
Ricardo Perez <rxperez12@gmail.com>
2024-05-28 15:47:40 -0700

    smoke test for card

---
+++ carousel/src/Card.test.jsx
@@ -0,0 +1,16 @@
+import { describe, it, test, expect } from "vitest";
+import React from "react";
+import { render } from "@testing-library/react";
+import Card from "./Card.jsx"; //TODO: do you need to add .jsx?
+import image1 from "./image1.jpg";
+
+test("render without crashing", function () {
+  render(
+    <Card
+      caption="hello"
+      src={image1}
+      currNum="1"
+      totalNum="1"
+    />,
+  );
+});
