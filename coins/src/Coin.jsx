9b3db84e063398f0a77caf92460de570445f617a
Ricardo Perez <rxperez12@gmail.com>
2024-05-28 17:31:18 -0700

    added coin template -WIP

---
+++ coins/src/Coin.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Coin.css";
+
+const COIN_IMAGES = {
+  heads: "./image1.png",
+  tails: "./image2.png",
+};
+
+/**
+ *
+ */
+function Coin({ sideShowing }) {
+  //OPTION1
+  //take sideShowing, and get file from coin_images
+  // return image of coin in div
+
+  return (
+    <div>
+      <img></img>
+    </div>
+  );
+}
