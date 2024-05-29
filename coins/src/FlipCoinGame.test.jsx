49a178185a9afae75392ecd9bab122e3f8db4671
Anya Aven <leafeon96@gmail.com>
2024-05-28 22:06:20 -0400

    Finished

--- coins/src/FlipCoinGame.test.jsx
+++ coins/src/FlipCoinGame.test.jsx
@@ -1,8 +1,44 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, vi, beforeAll, afterAll } from "vitest";
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import FlipCoinGame from "./FlipCoinGame.jsx";
+import Coin from "./Coin.jsx"
+import * as flip from "./flipCoin.js";
 
-test("renders without crashing", function () {
-  render(<FlipCoinGame/>);
+
+beforeAll(function () {
+  // observe calls & make it possible to mock
+  vi.spyOn(flip, "flipCoin");
+});
+
+afterAll(function () {
+  // after all tests, make dice.d6 normal again
+  //  (useful if other tests need this)
+  vi.restoreAllMocks();
+});
+
+describe("FlipCoinGame works", function () {
+
+  test("renders without crashing", function () {
+    render(<FlipCoinGame />);
+  });
+
+  test("renders 1 coin", function () {
+    const { container } = render(<Coin />);
+    expect(container.querySelectorAll(".Coin").length).toEqual(1);
+  });
+
+  test("Flips coin on button click", function () {
+    flip.flipCoin.mockReturnValueOnce(0);
+    const { container } = render(<FlipCoinGame />);
+    const qs = container.querySelector.bind(container);
+
+    const flipBtn = qs(".FlipCoin-flip-button");
+    fireEvent.click(flipBtn);
+
+    //expect the coin the be tails
+    expect(qs(".Coin-img")).toContainHTML("tailsPenny.jpg")
+    expect(qs(".Coin-img")).not.toContainHTML("headsPenny.jpg")
+  });
+
 });
