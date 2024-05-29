81cf4570472d6100ed774887755fdca3767cff83
Anya Aven <leafeon96@gmail.com>
2024-05-28 20:52:06 -0400

    Base WIP

--- coins/src/FlipCoinGame.jsx
+++ coins/src/FlipCoinGame.jsx
@@ -1,8 +1,16 @@
 import './FlipCoinGame.css';
 
-/**  */
+/**
+ * FlipCoinGame Component that holds game state for flipping coincs
+ *
+ * State:
+ * flips: {heads, tails}
+ *
+ * FlipCoinGame -> Coin
+*/
 function FlipCoinGame(){
 
+  return (<div className='FlipCoinGame'></div>)
 }
 
 export default FlipCoinGame;
