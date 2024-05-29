2d78cf1098d96935369be049ecd5fe0d10acc56d
Anya Aven <leafeon96@gmail.com>
2024-05-28 21:57:04 -0400

    Finished functionality

--- coins/src/FlipCoinGame.jsx
+++ coins/src/FlipCoinGame.jsx
@@ -1,23 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Coin from "./Coin.jsx";
 import "./FlipCoinGame.css";
+import { flipCoin } from "./flipCoin.js";
 
 /**
  * FlipCoinGame Component that holds game state for flipping coincs
  *
  * State:
  * flips: {heads, tails}
+ * coinState: default(null) => "heads", "tails"
  *
  * FlipCoinGame -> Coin
  */
 function FlipCoinGame() {
-  const [coinFlips, setCoinFlip] = useState();
+  const [flips, setFlips] = useState({ heads: 0, tails: 0 });
+  const [coinState, setCoinState] = useState(null);
 
-  // {head: 1, tails: 0}
+  function handleFlipClick(evt) {
+    const flipResult = flipCoin() === 0 ? "tails" : "heads";
+    const nextFlips = { ...flips };
+    nextFlips[flipResult] += 1;
+    setCoinState(flipResult);
+    setFlips(nextFlips);
+  }
+
   return (
     <div className="FlipCoinGame">
-      <Coin />
-      <button class="FlipCoin-button btn btn-primary">Flip Me!</button>
+      {coinState && <Coin sideShowing={coinState} />}
+      <button
+        className="FlipCoin-flip-button btn btn-primary"
+        onClick={handleFlipClick}>
+        Flip Me!
+      </button>
+
+      <p className="FlipCoinGame-Data">
+        {`Total flips:${flips.heads + flips.tails}
+        Heads: ${flips.heads}
+        Tails: ${flips.tails}`}
+      </p>
     </div>
   );
 }
